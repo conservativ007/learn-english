c3d52094500384fbae43486a46377c86792f675f
conservativ007 <alisa3d2007@gmail.com>
2022-06-06 18:15:25 +0300

    corrected mistakes in slider component

--- src/components/MySlider.jsx
+++ src/components/MySlider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef }from 'react';
+import React, { useState, useRef }from 'react';
 import { useParams } from 'react-router';
 import { AiFillSound } from 'react-icons/ai';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -7,6 +7,8 @@ import 'swiper/css';
 import "swiper/css/bundle";
 import { HiArrowNarrowLeft, HiArrowNarrowRight } from 'react-icons/hi';
 
+import { speech } from '../hooks/speech';
+
 const MySlider = () => {
 
   const swiperRef = useRef();
@@ -14,47 +16,50 @@ const MySlider = () => {
   const params = useParams();
   const name = params.card_name;
   const [cards] = useState(JSON.parse(localStorage.getItem(name)));
+  const [indexSlide, setIndexSlide] = useState(0);
 
   function isFlippedCardToggle(e) {
     if(e.target.className === "flip-card-front" || e.target.className === "flip-card-back")
     e.currentTarget.classList.toggle("is-flipped");
   }
 
-  return (
-    <>
-      <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      pagination={{ clickable: true }}
-      onSwiper={swiper => swiperRef.current = swiper}
-      onSlideChange={() => console.log('slide change')}
-      >
-        {
-          cards.map((item, index) => 
-            <SwiperSlide key={index}>
-              <div className="flip-card">
-                <div onClick={isFlippedCardToggle} className="flip-card-inner">
-                  <div className="flip-card-front">
-                    <div className="card-word">{item.word}</div>
-                    {/* <AiFillSound onClick={() => speech(cards.word)} className="sound" /> */}
+  if(cards) {
+    return (
+      <>
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        onSwiper={swiper => swiperRef.current = swiper}
+        onSnapIndexChange={(index) => setIndexSlide(index.activeIndex)}
+        >
+          {
+            cards.map((item, index) => 
+              <SwiperSlide key={index}>
+                <div className="flip-card">
+                  <div onClick={isFlippedCardToggle} className="flip-card-inner">
+                    <div className="flip-card-front">
+                      <div className="card-word">{item.word}</div>
+                      <AiFillSound onClick={() => speech(cards[indexSlide].word)} className="sound" />
+                    </div>
+                  <div className="flip-card-back">
+                    <div className="card-word_translate">{item.wordTranslate}</div>
+                    <div className="card-word_phrase">{item.phrase}</div>
+                    <AiFillSound onClick={() => speech(cards[indexSlide].phrase)} className="sound" />
                   </div>
-                <div className="flip-card-back">
-                  <div className="card-word_translate">{item.wordTranslate}</div>
-                  <div className="card-word_phrase">{item.phrase}</div>
-                  {/* <AiFillSound onClick={() => speech(cards[counter].phrase)} className="sound" /> */}
                 </div>
               </div>
-            </div>
-            </SwiperSlide>
-          )
-        }
-      </Swiper>
-      <div className="slider-arrows">
-        <HiArrowNarrowLeft className="arrow" onClick={() => swiperRef.current.slidePrev()} />
-        <HiArrowNarrowRight className="arrow" onClick={() => swiperRef.current.slideNext()} />
-      </div>
-      </>
-  );
+              </SwiperSlide>
+            )
+          }
+        </Swiper>
+        <div className="slider-arrows">
+          <HiArrowNarrowLeft className="arrow" onClick={() => swiperRef.current.slidePrev()} />
+          <div>{indexSlide + 1} / {cards.length}</div>
+          <HiArrowNarrowRight className="arrow" onClick={() => swiperRef.current.slideNext()} />
+        </div>
+        </>
+    );
+  }
 }
 
 export default MySlider;
--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router';
-import { HiArrowNarrowLeft, HiArrowNarrowRight } from 'react-icons/hi';
-import { AiFillSound } from 'react-icons/ai';
 import "../styles/show-card.css";
 import Header from './Header';
 import Games from './games/Games';
@@ -9,63 +7,16 @@ import Games from './games/Games';
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { speech } from '../hooks/speech';
 import MySlider from './MySlider';
 
 const ShowCard = () => {
-
   const params = useParams();
   const name = params.card_name;
-
-  const [cards] = useState(JSON.parse(localStorage.getItem(name)));
-  const [counter, setCouter] = useState(0);
-  
-  function isFlippedCardToggle(e) {
-    if(e.target.className === "flip-card-front" || e.target.className === "flip-card-back")
-    e.currentTarget.classList.toggle("is-flipped");
-  }
-
-  function goToTheNextCard() {
-    setCouter(counter + 1);
-    if(counter === cards.length - 1) {
-      setCouter(0);
-    }
-  }
- 
-  function goToThePrevCard() {
-    setCouter(counter - 1);
-    if(counter === 0) {
-      setCouter(cards.length - 1);
-    }
-  }
 
   return (
     <>
       <Header />
       <MySlider />
-      <div className="cards-content">
-    
-        {/* <div className="cards-container">
-          <div className="flip-card">
-            <div onClick={isFlippedCardToggle} className="flip-card-inner">
-              <div className="flip-card-front">
-                <div className="card-word">{cards[counter].word}</div>
-                <AiFillSound onClick={() => speech(cards[counter].word)} className="sound" />
-              </div>
-              <div className="flip-card-back">
-                <div className="card-word_translate">{cards[counter].wordTranslate}</div>
-                <div className="card-word_phrase">{cards[counter].phrase}</div>
-                <AiFillSound onClick={() => speech(cards[counter].phrase)} className="sound" />
-              </div>
-            </div>
-          </div>
-        </div> */}
-      </div>
-      {/* <div className="cards-control">
-        <HiArrowNarrowLeft className="arrow" onClick={goToThePrevCard} />
-        <div className="cards-counter">{counter + 1} / {cards.length}</div>
-        <HiArrowNarrowRight className="arrow" onClick={goToTheNextCard} />
-      </div> */}
       <Games setName={name} />
     </>
   );
